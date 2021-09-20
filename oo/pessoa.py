8a4a6b77a5389aea16e12ddb95228e7c25555d75
Pedro Lisboa <phlisboa2000@gmail.com>
2021-09-20 18:54:09 -0300

    Criado atributos de class!

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,14 +1,25 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    olhos = 2
+
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return 'Ola'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(p.cumprimentar())
-    print(p.nome)
+    olivia = Pessoa(nome='Olivia', idade=1)
+    pedro = Pessoa(olivia, nome='Pedro', idade=28)
 
+    print(pedro.filhos)
+    for filho in pedro.filhos:
+        print(filho.nome)
+
+    pedro.sobrenome = 'Lisboa'
+    print(pedro.sobrenome)
+
+    print(Pessoa.olhos)
+    print(pedro.olhos)
