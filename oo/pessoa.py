5e32c0129ef7e8b1efda20afd85c1472983e912c
Pedro Lisboa <phlisboa2000@gmail.com>
2021-09-20 18:13:17 -0300

    Criados atributos de instancia.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,3 +1,14 @@
 class Pessoa:
-    pass
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+
+    def cumprimentar(self):
+        return 'Ola'
+
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(p.cumprimentar())
+    print(p.nome)
 
